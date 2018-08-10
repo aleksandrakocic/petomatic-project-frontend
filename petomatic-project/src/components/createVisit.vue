83680b2c2766c2d22f7f7ee40b4889ce108a2ed9
aleksandrakocic <32391316+aleksandrakocic@users.noreply.github.com>
2018-08-10 15:55:40 +0200

    fix

---
+++ petomatic-project/src/components/createVisit.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div id='createVisitTable'>
+
+  </div>
+</template>
+<style>
+</style>
+
+<script>
+const axios = require('axios')
+export default {
+  name: 'createVisit',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+#createVisitTable {
+    width: 70vw;
+    float: left;
+    margin-top: 10vh;
+}
+
+</style>
